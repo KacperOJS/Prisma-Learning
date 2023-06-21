321c3dd685bafa2aff6f1b4d8756e31a42804dcd
Kacper <kacperodziemczyk@gmail.com>
2023-06-21 10:16:01 +0200

    Prisma Migration finished

--- script.ts
+++ script.ts
@@ -3,16 +3,16 @@ const prisma = new PrismaClient()
 
 // A `main` function so that we can use async/await
 async function main() {
-	const user =await prisma.user.findMany()
+	await prisma.user.deleteMany()
+	const user =await prisma.user.create({
+		data:{
+			name: "Kyle",
+			email:"rysiek@gmail.com",
+			age: 21,
+		},
+	})
 	console.log(user);
-	
-	// // Create a new user called `Alice`
-	// const newUser = await prisma.createUser({ name: 'Alice' })
-	// console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`)
-  
-	// // Read all users from the database and print them to the console
-	// const allUsers = await prisma.users()
-	// console.log(allUsers)
+
   }
   
   main()
